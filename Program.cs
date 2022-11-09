3b199c2cc6a42ce459097d2f76b0e07083eeb38e
unknown <msegantine8@gmail.com>
2022-11-09 10:00:35 -0300

    Initial Calculator

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿public class Calculator
+{
+    public int Sum(int ValueOne, int ValueTwo) => ValueOne + ValueTwo;
+
+    public int Subtraction(int ValueOne, int ValueTwo) => ValueOne - ValueTwo;
+
+    public int Multiplication(int ValueOne, int ValueTwo) =>  ValueOne * ValueTwo;
+
+    public int Division(int ValueOne, int ValueTwo) =>  ValueOne / ValueTwo;
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Digite o primeiro número");
+        int NumerOne = Int32.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite o segundo número");
+        int NumerTwo = Int32.Parse(Console.ReadLine());
+
+        Calculator Calc = new Calculator();
+
+        Console.Clear();
+
+        Console.WriteLine($"A soma dos valores é {Calc.Sum(NumerOne, NumerTwo)}");
+        Console.WriteLine($"A Subtração dos valores é {Calc.Subtraction(NumerOne, NumerTwo)}");
+        Console.WriteLine($"O produto dos valores é {Calc.Multiplication(NumerOne, NumerTwo)}");
+        Console.WriteLine($"A divisão dos valores é {Calc.Division(NumerOne, NumerTwo)}");
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+### Execute o seguinte comando para rodar o programa:
+`dotnet run`
+
+<br />
+<br />
+
+#### Este é um simples "Hello World" em C#. 
